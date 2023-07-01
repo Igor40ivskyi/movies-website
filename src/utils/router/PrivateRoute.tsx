e596687d6a687bcedde31a2f2b4040accb976cee
Admin <sorokivskyi777@gmail.com>
2023-07-01 10:16:31 +0300

    done the validators

--- src/utils/router/PrivateRoute.tsx
+++ src/utils/router/PrivateRoute.tsx
@@ -1,12 +1,16 @@
 import {authService} from "../../services";
-import {IndexRouteProps, Navigate, Outlet} from "react-router-dom";
-import {FC} from "react";
+import {Navigate, Outlet} from "react-router-dom";
+import jwtDecode from "jwt-decode";
 
 const PrivateRoute = () => {
 
-
     const accessToken = authService.getAccessToken();
 
+    try {
+        jwtDecode(authService.getAccessToken());
+    }catch (e) {
+        return <Navigate to={'/login'}/>;
+    }
 
     return (
         accessToken ? <Outlet/> : <Navigate to={'/login'}/>
