52b0e42f46c2870316401556e4a58d8ca386fdee
Admin <sorokivskyi777@gmail.com>
2023-07-04 12:51:07 +0300

    done the genres

--- src/components/ShowAuth/ShowAuth.tsx
+++ src/components/ShowAuth/ShowAuth.tsx
@@ -1,9 +1,16 @@
-
+import './ShowAuth.css';
+import {NavLink} from "react-router-dom";
 
 const ShowAuth = () => {
     return (
         <div className={'authContainer'}>
-            <div className={'authLabel'}>AUTH</div>
+            <div className={'authLabel'}>Auth</div>
+
+            <div className={'authlist'}>
+                    <NavLink className={'auth__link'} to={'register'}>Register</NavLink>
+                    <NavLink className={'auth__link'} to={'login'}>Login</NavLink>
+            </div>
+
         </div>
     );
 };
