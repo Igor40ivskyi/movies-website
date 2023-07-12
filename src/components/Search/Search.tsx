329b5efbda39139094576a4b7afab94cc197a959
Admin <sorokivskyi777@gmail.com>
2023-07-12 15:16:54 +0300

    done the genres

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,18 +1,35 @@
 import './Search.css';
+import {useState} from "react";
 
 const Search = () => {
+
+    const [searchValue, setSearchValue] = useState<string>('');
+
+    const handleInputChange = (event: any) => {
+        setSearchValue(event.target.value);
+    };
+
+    const handleClearClick = () => {
+        setSearchValue('');
+    };
+
     return (
         <div className={'search_container'}>
 
-            <div  className={'search'} id={'sss'}>
+            <div className={'search'} id={'sss'}>
+
                 <div onClick={() => {
                     const div = document.getElementById('sss');
-                    console.log(div);
                     div.classList.toggle('active');
                 }} className={'icon'}>
-                    <img src="https://w7.pngwing.com/pngs/668/951/png-transparent-magnifying-glass-computer-icons-encapsulated-postscript-magnifying-glass-magnifier-tool-terminal-icon.png" alt="search"/>
                 </div>
-                <input type="text" placeholder={'search'}/>
+
+                <div className={'input'}>
+                    <input value={searchValue} onChange={handleInputChange} type="text" placeholder={'search'}
+                           id={'input1'}/>
+                </div>
+
+                <span onClick={handleClearClick} className={'clear'}></span>
             </div>
 
         </div>
